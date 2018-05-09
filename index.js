cf6e10571368f8e6483db9de5e91b07d9b12c8ba
Chri <chri@localhost.localdomain>
2018-05-09 11:28:34 +0200

    constructor

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-class RockPapaerScissor{
-    constructor(){
+class RockPaperScissor{
+    constructor(containerEl){
+        const mainDiv = document.createElement("div");
+        const title = document.createElement("h1");
+        title.textContent = "Your Game";
+        
+        const button = document.createElement("button");
+        mainDiv.appendChild(title);
+        mainDiv.appendChild(button);
+        button.textContent = "Play!";
+        //const radio0 = document.createElement("input").setAttribute("type", "radio").setAttribute("name", "choice").setAttribute("value", "0");
 
+        containerEl.append(mainDiv);
     }
-
 }
