fb815f2bc0a85d2266d12c229fe98db44d42df8d
Chri <chri@localhost.localdomain>
2018-05-09 12:19:05 +0200

    edited

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 class RockPaperScissor{
-<<<<<<< HEAD
     constructor(containerEl){
         const mainDiv = document.createElement("div");
         const title = document.createElement("h1");
         title.textContent = "Your Game";
+        mainDiv.append(title);
+
+        let radios = [];
+
+        const labels = ["Rock", "Paper", "Scissor"];
+
+        for(let i = 0; i < 3; i++) {
+            const radio = document.createElement("input");
+            radio.setAttribute("type", "radio");
+            radio.setAttribute("name", "choice");
+            radio.setAttribute("id", i);
+            radio.setAttribute("value", i);
+            radios.push(radio);
+
+            const label = document.createElement("label");
+            label.setAttribute("for", i);
+            label.textContent = labels[i];
+            let br = document.createElement("br");
+
+            mainDiv.append(radio);
+            mainDiv.append(label);
+            mainDiv.append(br);
+        }
         
         const button = document.createElement("button");
-        mainDiv.appendChild(title);
-        mainDiv.appendChild(button);
         button.textContent = "Play!";
-        //const radio0 = document.createElement("input").setAttribute("type", "radio").setAttribute("name", "choice").setAttribute("value", "0");
-=======
-    constructor(){
->>>>>>> f514e84965aab04d4a24af5ba7241fa322dc7027
+
+        mainDiv.append(button);
 
         containerEl.append(mainDiv);
     }
-<<<<<<< HEAD
-=======
+    
     opponentChoice(){
         this.opponent_choice= Math.floor(Math.random()*3);
     }
 
->>>>>>> f514e84965aab04d4a24af5ba7241fa322dc7027
 }
